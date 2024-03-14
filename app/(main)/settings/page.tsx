db746b7145541f6cda3d1b60e97baba23fdce30b
Roland Wenzlofsky <r.wenzlofsky@gmail.com>
2024-03-14 22:19:29 +0000

    wip

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,23 +1,13 @@
-"use client";
 
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 
 export const WhoopForm = () => {
 
-  const handleSubmit = () => {
-    toast.info("Submit triggered");
-
-  };
 
   return (
     
-      
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <Button type="submit"  className="w-full">
-            Login Whoop
-          </Button>
-        </form>
+    <h1>Hallo</h1>      
       
   );
   }
