9f4a15242c77c94b8c657b83cac96df5e260accb
Roland Wenzlofsky <r.wenzlofsky@gmail.com>
2024-03-14 19:54:27 +0000

    WIP

--- auth/config.ts
+++ auth/config.ts
@@ -1,6 +1,6 @@
-import { CredentialsProvider, GithubProvider, GoogleProvider } from "@/auth/providers";
+import { CredentialsProvider, GoogleProvider } from "@/auth/providers";
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
-  providers: [CredentialsProvider, GithubProvider, GoogleProvider],
+  providers: [CredentialsProvider, GoogleProvider],
 } satisfies NextAuthConfig;
--- auth/providers.ts
+++ auth/providers.ts
@@ -1,7 +1,6 @@
 import { loginSchema } from "@/schemas";
 import { getUserByEmail } from "@/services/user";
 import Credentials from "next-auth/providers/credentials";
-import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 
@@ -24,10 +23,6 @@ export const CredentialsProvider = Credentials({
   },
 });
 
-export const GithubProvider = Github({
-  clientId: process.env.GITHUB_ID as string,
-  clientSecret: process.env.GITHUB_SECRET as string,
-});
 
 export const GoogleProvider = Google({
   clientId: process.env.GOOGLE_ID as string,
