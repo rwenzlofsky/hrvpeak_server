00caca462addbbae8786b11793cf44f23fc81295
Roland Wenzlofsky <r.wenzlofsky@gmail.com>
2024-03-14 22:17:52 +0000

    wip

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { CardWrapper } from "@/components/auth/card-wrapper";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form } from "@/components/ui/form";
-import { z } from "zod";
-import { loginSchema } from "@/schemas";
 import { Button } from "@/components/ui/button";
-import { useTransition } from "react";
 import { toast } from "sonner";
-import { useRouter } from "next/navigation";
 
 export const WhoopForm = () => {
 
@@ -22,9 +14,6 @@ export const WhoopForm = () => {
     
       
         <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="space-y-4">
-            
-          </div>
           <Button type="submit"  className="w-full">
             Login Whoop
           </Button>
